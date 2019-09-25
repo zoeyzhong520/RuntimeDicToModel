0a9521090e83712105ab4bd8554abe02e122c699
AsiaZhang <17612156438@sina.cn>
2019-09-25 11:31:29 +0800

    init

---
+++ 实现字典转模型/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  实现字典转模型
+//
+//  Created by zhifu360 on 2019/9/25.
+//  Copyright © 2019 ZZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 实现字典转模型/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  实现字典转模型
+//
+//  Created by zhifu360 on 2019/9/25.
+//  Copyright © 2019 ZZJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
